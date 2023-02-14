063d86ff0415eb52d9f6cfecbfdc2d930d5adeb1
ziaulkadri <ziaul.kadri@innoage.in>
2023-02-14 23:42:14 +0530

    added more users

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -4,7 +4,28 @@ import UsersList from '../components/UsersList';
 const Users = () => {
 	const USERS = [
 		{
-			id: 'u!',
+			id: 'u1',
+			name: 'MAx schwarz',
+			image:
+				'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2080&q=80',
+			places: 3,
+		},
+		{
+			id: 'u2',
+			name: 'MAx schwarz',
+			image:
+				'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2080&q=80',
+			places: 3,
+		},
+		{
+			id: 'u3',
+			name: 'MAx schwarz',
+			image:
+				'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2080&q=80',
+			places: 3,
+		},
+		{
+			id: 'u4',
 			name: 'MAx schwarz',
 			image:
 				'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2080&q=80',
