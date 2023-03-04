d8d10cdc0ce347cd260d90171f2e8964e18db66c
ziaulkadri <ziaul.kadri@innoage.in>
2023-03-04 21:25:48 +0530

    UI:added google maps

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -3,6 +3,7 @@ import './PlaceItem.css';
 import Card from '../../shared/UIElements/Card';
 import Button from '../../shared/FormElements/Button';
 import Modal from '../../shared/UIElements/Modal';
+import Map from '../../shared/UIElements/Map';
 
 const PlaceItem = (props) => {
 	const [showMap, setShowMap] = useState(false);
@@ -26,7 +27,7 @@ const PlaceItem = (props) => {
 				footer={<Button onClick={closeMapHandler}> Close</Button>}
 			>
 				<div className="map-container">
-					<h2>THE MAP</h2>
+					<Map center={props.coordinates} zoom={16} />
 				</div>
 			</Modal>
 			<li className="place-item">
