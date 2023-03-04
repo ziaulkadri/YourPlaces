5b5a0e4f7de8f42d92d232c29888ab938d14cb46
ziaulkadri <ziaul.kadri@innoage.in>
2023-03-04 22:18:47 +0530

    UI:added resuable input component

--- src/places/pages/NewPlaces.js
+++ src/places/pages/NewPlaces.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Input from '../../shared/FormElements/Input';
+import './NewPlaces.css';
 const NewPlaces = () => {
 	return (
-		<div>
-			<button>NewPlaces</button>
-		</div>
+		<form className="place-form">
+			<Input element="input" type="text" label="Title" />
+		</form>
 	);
 };
 
