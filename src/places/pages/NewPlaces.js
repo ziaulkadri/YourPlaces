250be8f8e7e558517615fde5f06fa6380a48305b
ziaulkadri <ziaul.kadri@innoage.in>
2023-03-04 22:49:54 +0530

    UI:added useReducer for handling onChange in input

--- src/places/pages/NewPlaces.js
+++ src/places/pages/NewPlaces.js
@@ -4,7 +4,13 @@ import './NewPlaces.css';
 const NewPlaces = () => {
 	return (
 		<form className="place-form">
-			<Input element="input" type="text" label="Title" />
+			<Input
+				element="input"
+				type="text"
+				label="Title"
+				validators={[]}
+				errorText="Please enter a valid title"
+			/>
 		</form>
 	);
 };
